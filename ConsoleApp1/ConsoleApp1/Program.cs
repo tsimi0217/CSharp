b8ee6260225ed2199267787ef1460c6d9de0e04f
tsimi0217 <39930369+tsimi0217@users.noreply.github.com>
2018-06-11 21:47:13 -0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money = 100000;
+            int year = 1;
+            float x = 3.0f;
+            double y = 4.5d;
+            bool yes = true;
+            Console.WriteLine(money);
+            Console.WriteLine(year);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(yes);
+            Console.ReadKey();
+
+            Console.WriteLine("I want to earn {0} dollars the {1}st year I am employed. I hope to work at least {2} years there.", money, year, x);
+            Console.ReadKey();
+
+            string thoughts = "I like C# better than JavaScript.";
+            Console.WriteLine(thoughts);
+            Console.ReadKey();
+
+            Console.Write("Please enter in your desired job after i.c.stars:");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.ReadKey();
+
+            Console.WriteLine(10 > 100);
+            Console.WriteLine("pineapple" == "apple");
+            Console.WriteLine("sunshine" == "sunshine");
+            Console.WriteLine(2516 > 2000);
+            Console.ReadKey();
+
+
+
+        }
+    }
+    
+}

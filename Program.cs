3ccc888ce6dd1b16d9467c3906487816327d4d31
tsimi0217 <39930369+tsimi0217@users.noreply.github.com>
2018-06-14 14:18:51 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _6._14._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greeting();
+            int num = returnAnInt(35, 105);
+            Console.WriteLine(num);
+            Learning();
+            Fun();
+            Console.ReadKey();
+
+        }
+
+        public static void Greeting ()
+        {
+            Console.WriteLine("Good Morning!");
+        }
+
+        private static void Learning()
+        {
+            Console.WriteLine("C# is Fun.");
+        
+
+        }
+
+        public static int returnAnInt(int x, int y)
+        {
+            
+            return x-y;            
+        }
+
+        public static void Fun ()
+        {
+            Console.WriteLine("Creating C# is interesting.");
+        }
+    }
+}
+
+
+
+
